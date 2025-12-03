893edfd626e6cda0ca33409fe555bde6c7f3f3bb
vanjastankovic00 <vanjastankovic00@gmail.com>
2025-12-03 13:12:55 +0100

    Update to v2 of Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Button, ButtonGroup, Show } from "@chakra-ui/react"
+import { Grid, GridItem } from "@chakra-ui/react"
 
 function App() {
   return (
     <div>
-      <Button colorPalette= "blue" onClick={() => console.log("Clicked!!")}>Click me!</Button>
+      <Grid templateAreas={{
+        base : `"nav" "main"`,
+        lg: `"nav nav" "aside main"` // 1024px
+      }}>
+        <GridItem area= "nav" bg = "coral">Nav</GridItem>
+        <Show above="lg">
+          <GridItem area= "aside" bg = "gold">Aside</GridItem>
+        </Show>
+        <GridItem area= "main" bg = "dodgerblue">Main</GridItem>
+      </Grid>
+      {/*<Button colorPalette= "blue" onClick={() => console.log("Clicked!!")}>Click me!</Button> */}
     </div>
+
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { defaultSystem } from "@chakra-ui/react"
 import { Provider } from "@/components/ui/provider"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider value = {defaultSystem}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
