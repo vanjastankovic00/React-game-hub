cd9048c44409dee71fcf74093e8240c8ce8cf50a
vanjastankovic00 <vanjastankovic00@gmail.com>
2025-12-03 13:34:52 +0100

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button, ButtonGroup, Show } from "@chakra-ui/react"
 import { Grid, GridItem } from "@chakra-ui/react"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
         base : `"nav" "main"`,
         lg: `"nav nav" "aside main"` // 1024px
       }}>
-        <GridItem area= "nav" bg = "coral">Nav</GridItem>
+        <GridItem area= "nav">
+          <NavBar />
+        </GridItem>
         <Show above="lg">
           <GridItem area= "aside" bg = "gold">Aside</GridItem>
         </Show>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Provider } from "@/components/ui/provider"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
